02736d5d62d08f19440a38a9bb6b60a52c64c101
HystericalDragon <138737572+HystericalDragon@users.noreply.github.com>
2023-08-31 09:31:55 +0800

    fix(buildScript): automatic exit and set defatult dashboard

--- buildScript/dashboard/init.sh
+++ buildScript/dashboard/init.sh
@@ -3,6 +3,7 @@
 # Just support MetaCubexd :)
 # Now you can use metacubexd or Yacd-meta
 DASH_REPO="$1"
+[ -z "$DASH_REPO" ] && DASH_REPO="metacubexd"
 DIST_DIR="Dash-${DASH_REPO}"
 
 pnpm -v || exit 1
@@ -11,7 +12,7 @@ pushd ..
 
 
 if [ ! -d "$DASH_REPO" ]; then
-  git clone https://github.com/MetaCubeX/${DASH_REPO}.git
+  git clone https://github.com/MetaCubeX/${DASH_REPO}.git || exit 1
 fi
 
 pushd ${DASH_REPO}/
@@ -25,5 +26,5 @@ popd
 
 popd
 
-cp ../${DASH_REPO}/dashboard.zip app/src/main/assets/dashboard.zip
+cp ../${DASH_REPO}/dashboard.zip app/src/main/assets/dashboard.zip || exit 1
 echo ">> install $DASH_REPO to  app/src/main/assets/dashboard.zip"
